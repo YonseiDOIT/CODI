664be2574a6c8f7804282247c0036dacbf2f0c86
haeunnnn <haeun234559@gmail.com>
2024-11-26 21:37:45 +0900

    Add - color theme, custom icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// data
+import 'package:codi/data/theme.dart';
+import 'package:codi/data/custom_icons.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(),
+      // theme: ThemeData(),
+      theme: mainTheme,
       home: const Main(),
       debugShowCheckedModeBanner: false,
     );
@@ -23,6 +28,110 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8),
+          padding: const EdgeInsets.all(5),
+          children: [
+            Icon(
+              Custom_Icons.home,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.chatbubbles,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.award_1,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.profile_1,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.profile_2,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.message,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.lock,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.search,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.notification,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.notificationNew,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.skillTitle,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.award_2,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.calender,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.send,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.sending,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.microphone,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.add,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.chevronDown,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.bookmark,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.edit,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.filter,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.left,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            Icon(
+              Custom_Icons.right,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
