7f8fdb59b1893cd510314d85369fc7ea1119ffa6
JH <0john.hong0@gmail.com>
2024-12-02 10:19:18 +0900

    Modify - api test

--- lib/data/api_test.dart
+++ lib/data/api_test.dart
@@ -1,7 +1,10 @@
 import 'api_wrapper.dart' as api;
+import 'globals.dart' as globals;
 
 void main() async {
-  Map result = await api.User.addUser();
+  // await dotenv.load(fileName: 'assets/env/.env');
+  // globals.backendKey = dotenv.get("backendKey");
 
+  Map result = await api.User.getUser(user_id: 1);
   print(result);
 }
