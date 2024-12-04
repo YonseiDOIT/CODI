3f6bab81b0adc96d70d113e304c1adee3202028e
JH <0john.hong0@gmail.com>
2024-12-04 15:09:12 +0900

    Modify - globals merge size_config file to globals file

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -1,4 +1,29 @@
 library globals;
 
-// String backendKey = "7c2ae4be5eae166c442b8e74919eda77253a1cfd0fa7f8fcafd984f0b2cdc0e7";
+import 'package:flutter/material.dart';
+
 late String backendKey;
+
+class ScreenSize {
+  static late Size size;
+  static late double height;
+  static late double width;
+  static late double pixelRatio;
+  static late double topPadding;
+  static late double bottomPadding;
+
+  // MediaQuery.of(context).size             //앱 화면 크기 size  Ex> Size(360.0, 692.0)
+  // MediaQuery.of(context).size.height      //앱 화면 높이 double Ex> 692.0
+  // MediaQuery.of(context).size.width       //앱 화면 넓이 double Ex> 360.0
+  // MediaQuery.of(context).devicePixelRatio //화면 배율    double Ex> 4.0
+  // MediaQuery.of(context).padding.top      //상단 상태 표시줄 높이 double Ex> 24.0
+
+  void initSizes(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    topPadding = MediaQuery.of(context).padding.top;
+    bottomPadding = MediaQuery.of(context).padding.bottom;
+  }
+}
