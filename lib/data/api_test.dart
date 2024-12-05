ae91643df261a24231ffa26d65f4e05ce3587713
JH <0john.hong0@gmail.com>
2024-12-05 10:29:32 +0900

    Add - get contest list

--- lib/data/api_test.dart
+++ lib/data/api_test.dart
@@ -5,6 +5,10 @@ void main() async {
   // await dotenv.load(fileName: 'assets/env/.env');
   // globals.backendKey = dotenv.get("backendKey");
 
-  Map result = await api.User.getUser(user_id: 1);
-  print(result);
+  globals.backendKey = "7c2ae4be5eae166c442b8e74919eda77253a1cfd0fa7f8fcafd984f0b2cdc0e7";
+
+  // Map result = await api.User.getUser(user_id: 1);
+  List result = await api.Contest.getContests();
+  print(result.length);
+  print(result[0]["name"]);
 }
