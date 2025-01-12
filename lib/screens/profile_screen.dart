5d736f2326e075a6f2fd0c96e90584acaa79de87
haeunnnn <haeun234559@gmail.com>
2025-01-12 21:15:06 +0900

    Modify - merge main into login

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import 'dart:ui';
 
 import 'package:codi/widgets/post.dart';
-=======
-import 'package:codi/data/custom_icons.dart';
->>>>>>> 28c987fe7be4043fadf67d125b5bbd343f94942f
 import 'package:flutter/material.dart';
 
 import 'package:codi/data/theme.dart';
