a10017d00e1451b569b031c774bd475fd6f34357
haeunnnn <haeun234559@gmail.com>
2025-01-18 11:39:14 +0900

    Modify - new profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: child!,
+        );
+      },
       // theme: ThemeData(),
       theme: mainTheme,
       home: Main(),
