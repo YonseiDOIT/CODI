234dc9e049736861c2377574207510c5bdf5ca2c
JH <0john.hong0@gmail.com>
2025-01-31 21:05:44 +0900

    Add - defaultTitles

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -2,13 +2,24 @@ library globals;
 
 import 'package:flutter/material.dart';
 import 'package:codi/data/shared_pref.dart';
-import 'package:codi/models/models.dart';
+import 'package:codi/models/models.dart' as models;
 
 late String backendKey;
 
 bool isLoggedIn = false;
 Preferences localData = Preferences();
-late User codiUser;
+late models.User codiUser;
+
+final List<models.Title> defaultTitles = [
+  models.Title(title_id: 1, title: "지혜로운 해결사", image_name: "wise_troubleshooter", type: "positive", count: 0),
+  models.Title(title_id: 2, title: "감각적인 디자이너", image_name: "sensual_designer", type: "positive", count: 0),
+  models.Title(title_id: 3, title: "프로 개발자", image_name: "professional_developer", type: "positive", count: 0),
+  models.Title(title_id: 4, title: "열정적인 중재자", image_name: "passionate_mediator", type: "positive", count: 0),
+  models.Title(title_id: 5, title: "아이디어 뱅크", image_name: "idea_bank", type: "positive", count: 0),
+  models.Title(title_id: 6, title: "브레인", image_name: "brain", type: "positive", count: 0),
+  models.Title(title_id: 7, title: "발표의 신", image_name: "god_of_presentation", type: "positive", count: 0),
+  models.Title(title_id: 8, title: "정리의 신", image_name: "god_of_organization", type: "positive", count: 0),
+];
 
 class Colors {
   static const Color point1 = Color(0xFF6B27FE);
