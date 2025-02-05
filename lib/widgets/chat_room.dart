2000ab53cb141b8cdbb6471aae8e73f1dff0ead1
haeunnnn <haeun234559@gmail.com>
2025-02-06 00:02:07 +0900

    Modify - peer review screen

--- lib/widgets/chat_room.dart
+++ lib/widgets/chat_room.dart
@@ -17,7 +17,8 @@ List<models.ChatMessage> messages = [
       user_id: 1,
       username: "username",
       email: "email",
-      profile_picture: "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+      profile_picture:
+          "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
       position: "FE",
       gender: 0,
     ),
@@ -26,13 +27,15 @@ List<models.ChatMessage> messages = [
     message_id: 2,
     room_id: 1,
     user_id: 2,
-    content: "네 안녕하세요. 저는 계정이 없는 유저입니다. 이 계정은 테스트를 위해 만든 존재하지 않는 계정입니다. DB에 없고 API로도 불러올 수 없는 계정입니다.",
+    content:
+        "네 안녕하세요. 저는 계정이 없는 유저입니다. 이 계정은 테스트를 위해 만든 존재하지 않는 계정입니다. DB에 없고 API로도 불러올 수 없는 계정입니다.",
     sent_at: DateTime(2025, 1, 22, 15, 31),
     user: models.User(
       user_id: 2,
       username: "username",
       email: "email",
-      profile_picture: "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+      profile_picture:
+          "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
       position: "FE",
       gender: 0,
     ),
@@ -41,13 +44,15 @@ List<models.ChatMessage> messages = [
     message_id: 3,
     room_id: 1,
     user_id: 2,
-    content: "그저 테스트를 위해 만든 User 모델이라는 것만 알고 계시면 됩니다. 더 할말이 없지만 길이가 길어졌을떄 어떻게 보일지 확인해야해서 길게 쓰고 있습니다. 룰루랄라",
+    content:
+        "그저 테스트를 위해 만든 User 모델이라는 것만 알고 계시면 됩니다. 더 할말이 없지만 길이가 길어졌을떄 어떻게 보일지 확인해야해서 길게 쓰고 있습니다. 룰루랄라",
     sent_at: DateTime(2025, 1, 22, 15, 32),
     user: models.User(
       user_id: 2,
       username: "username",
       email: "email",
-      profile_picture: "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+      profile_picture:
+          "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
       position: "FE",
       gender: 0,
     ),
@@ -56,13 +61,15 @@ List<models.ChatMessage> messages = [
     message_id: 1,
     room_id: 1,
     user_id: 1,
-    content: "이 메시지는 테스트를 위해 만든 메시지입니다. 이 메시지는 ChatRoom 위젯을 눌러서 들어온 ChatRoomScreen 위젯에서 보여집니다.",
+    content:
+        "이 메시지는 테스트를 위해 만든 메시지입니다. 이 메시지는 ChatRoom 위젯을 눌러서 들어온 ChatRoomScreen 위젯에서 보여집니다.",
     sent_at: DateTime(2025, 1, 22, 16, 0),
     user: models.User(
       user_id: 1,
       username: "username",
       email: "email",
-      profile_picture: "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+      profile_picture:
+          "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
       position: "FE",
       gender: 0,
     ),
@@ -77,7 +84,8 @@ List<models.ChatMessage> messages = [
       user_id: 3,
       username: "username2",
       email: "email",
-      profile_picture: "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
+      profile_picture:
+          "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
       position: "FE",
       gender: 0,
     ),
@@ -100,7 +108,45 @@ class ChatRoom extends StatelessWidget {
                 name: 'asdf',
                 messages: List.from(messages.reversed),
               ),
-              chatRoomUsers: [],
+              chatRoomUsers: [
+                globals.codiUser,
+                models.User(
+                  user_id: 2,
+                  username: "username",
+                  email: "email",
+                  profile_picture:
+                      "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+                  position: "FE",
+                  gender: 0,
+                ),
+                models.User(
+                  user_id: 3,
+                  username: "username2",
+                  email: "email",
+                  profile_picture:
+                      "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
+                  position: "FE",
+                  gender: 0,
+                ),
+                models.User(
+                  user_id: 3,
+                  username: "username2",
+                  email: "email",
+                  profile_picture:
+                      "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
+                  position: "FE",
+                  gender: 0,
+                ),
+                models.User(
+                  user_id: 3,
+                  username: "username2",
+                  email: "email",
+                  profile_picture:
+                      "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
+                  position: "FE",
+                  gender: 0,
+                ),
+              ],
             ),
           ),
         );
@@ -119,7 +165,8 @@ class ChatRoom extends StatelessWidget {
                   user_id: 2,
                   username: "username",
                   email: "email",
-                  profile_picture: "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+                  profile_picture:
+                      "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
                   position: "FE",
                   gender: 0,
                 ),
@@ -127,7 +174,8 @@ class ChatRoom extends StatelessWidget {
                   user_id: 3,
                   username: "username2",
                   email: "email",
-                  profile_picture: "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
+                  profile_picture:
+                      "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
                   position: "FE",
                   gender: 0,
                 ),
@@ -135,7 +183,8 @@ class ChatRoom extends StatelessWidget {
                   user_id: 2,
                   username: "username",
                   email: "email",
-                  profile_picture: "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
+                  profile_picture:
+                      "https://cdn.pixabay.com/photo/2021/09/20/03/24/skeleton-6639547_1280.png",
                   position: "FE",
                   gender: 0,
                 ),
@@ -143,7 +192,8 @@ class ChatRoom extends StatelessWidget {
                   user_id: 3,
                   username: "username2",
                   email: "email",
-                  profile_picture: "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
+                  profile_picture:
+                      "https://i.pinimg.com/236x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg",
                   position: "FE",
                   gender: 0,
                 ),
